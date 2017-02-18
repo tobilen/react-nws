c320b15bc9ddc5e1ec1ee64903bf6e8e68360086
Tobias Lengsholz <t.lengsholz@neusta.de>
2017-02-18 18:55:25 +0100

    enabled editable props

--- stories/index.js
+++ stories/index.js
@@ -1,33 +1,45 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
+import { withKnobs, text, boolean, number } from '@kadira/storybook-addon-knobs';
 import Button from './../app/components/Button';
 import Heading from './../app/components/Heading';
 
-
 storiesOf('Button', module)
+  .addDecorator(withKnobs)
   .addWithInfo(
-    'with label',
-    `Basic Usage of Button`,
+    'Basic',
+    `Basic Usage of a Link Element.`,
     () => (
-    <Button onClick={action('clicked')} label="Hello Button">
-      Child Element
+    <Button href={text("href", "http://google.com")}>
+      {text("Label", "Basic Usage")}
     </Button>
   ))
-  .add('with href', () => (
-    <Button href="http://google.com" onClick={action('clicked')} label="Hello Button">
-      Child Element
+  .addWithInfo(
+    'with Icon',
+    `Link Element with Icon. By default, icons will be positioned before element children`,
+    () => (
+    <Button href={text("href", "http://google.com")} icon={text("icon", "overview")}>
+      {text("Label", "Link with Icon")}
     </Button>
   ))
-  .add('with children', () => (
+  .addWithInfo(
+    'with trailing icon',
+    `Link Element with Icon. To override default positioning, icon-position parameter can be specified`,
+    () => (
+    <Button href={text("href", "http://google.com")} icon={text("icon", "external")} icon-trailing={boolean("Trailing Icon", true)}>
+      {text("Label", "Link with trailing Icon")}
+    </Button>
+  ))
+  .addWithInfo(
+    'with onClick listener',
+    `Instead of an href, an onClick listener can be specified. In this case, a button will be rendered out instead of an a.`,
+    () => (
     <Button onClick={action('clicked')}>
-      Child Element
-    </Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')} label="">
-      😀 😎 👍 💯
+      {text("Label", "Link with onClick listener")}
     </Button>
   ));
+
+
 
 storiesOf('Heading', module)
   .add('1', () => (
