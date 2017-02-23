509a602cb7d36297bfe67214616b2555e9173e35
Tobias Lengsholz <t.lengsholz@neusta.de>
2017-02-23 17:49:09 +0100

    extended readme

--- app/components/Button/tests/index.test.js
+++ app/components/Button/tests/index.test.js
@@ -8,7 +8,7 @@ import { mount } from 'enzyme';
 import Button from '../index';
 
 const handleRoute = () => {};
-const href = 'http://mxstbr.com';
+const href = 'http://google.com';
 const children = (<h1>Test</h1>);
 const renderComponent = (props = {}) => mount(
   <Button href={href} {...props}>
